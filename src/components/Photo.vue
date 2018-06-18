69a558b3e0ecac2fcdc303e18cc5578a3cf662d6
yamaguchi arata <arata@yamaguchi-no-iMac.local>
2018-06-19 02:05:05 +0900

    bootstrap import

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,8 +1,8 @@
 <template>
  <div>
-    <h1>PHOTO hoge</h1>
-    <div id="next-button"><button v-on:click="imageCntUp()">NEXT</button></div>
+    <h1>PHOTO</h1>
     <img :src="imagePath">
+    <div id="next-button"><button v-on:click="imageCntUp()">NEXT</button></div>
  </div>
 </template>
 
@@ -12,12 +12,12 @@ export default {
     return {
       cnt: 0,
       images: ['hobby01', 'hobby02', 'hobby03', 'hobby04'],
-      image_src: require('./../assets/hobby01.jpg')
+      image_src: require('./../../static/assets/hobby01.jpg')
     }
   },
   computed: {
     imagePath () {
-      return '/static/assets/' + this.images[this.cnt] + '.jpg'
+      return './../../static/assets/' + this.images[this.cnt] + '.jpg'
     }
   },
   methods: {
