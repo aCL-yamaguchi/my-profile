b6ac5d1ad2a67684efe9387cf79cfdaf6227fa95
yamaguchi arata <arata@yamaguchi-no-iMac.local>
2018-06-19 01:24:00 +0900

    Photo.vue 画像表示ロジック修正

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,13 +1,8 @@
 <template>
  <div>
-    <h1>PHOTO</h1>
-    <div id="next-button"><button v-on:click="nextPhoto()">NEXT</button></div>
-    <img :src="image_src">
-    <!--
-    <img src="../assets/hobby01.jpg" alt="hobby01">
-    <img src="../assets/hobby02.jpg" alt="hobby02">
-    <img src="../assets/hobby03.jpg" alt="hobby03">
-    <img src="../assets/hobby04.jpg" alt="hobby04">-->
+    <h1>PHOTO hoge</h1>
+    <div id="next-button"><button v-on:click="imageCntUp()">NEXT</button></div>
+    <img :src="imagePath">
  </div>
 </template>
 
@@ -15,27 +10,20 @@
 export default {
   data () {
     return {
+      cnt: 0,
+      images: ['hobby01', 'hobby02', 'hobby03', 'hobby04'],
       image_src: require('./../assets/hobby01.jpg')
     }
   },
+  computed: {
+    imagePath () {
+      return '/static/assets/' + this.images[this.cnt] + '.jpg'
+    }
+  },
   methods: {
-    // 表示画像切り替え
-    nextPhoto: function (event) {
-      // todo配列を動的に生成
-      const photoNameArray = ['hobby01', 'hobby02', 'hobby03', 'hobby04']
-      const MAX_INDEX = 3
-
-      photoNameArray.forEach(function (element) {
-        let index = photoNameArray.indexOf(element)
-        if (index >= 0) {
-          let nextIndex = index + 1
-          // 次インデックスが要素数より大きい場合は、一番先頭の画像を表示する
-          if (MAX_INDEX < nextIndex) {
-            nextIndex = 0
-          }
-          this.image_src = '../assets/' + photoNameArray[nextIndex] + '.jpg'
-        }
-      })
+    imageCntUp () {
+      this.cnt++
+      if (this.cnt >= this.images.length) this.cnt = 0
     }
   }
 }
