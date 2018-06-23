958133ec5e50fc32857b20751d53e4a5d69279d3
yamaguchi arata <arata@yamaguchi-no-iMac.local>
2018-06-23 23:50:38 +0900

    add  dropdownmenu

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="dropdown-menu" v-on:click="showMenu()">
+  <ul>
+    <router-link tag="li" to="/about">
+      <a>ABOUT</a>
+    </router-link>
+    <router-link tag="li" to="/photo">
+      <a>PHOTO</a>
+    </router-link>
+    <router-link tag="li" to="/contact">
+      <a>CONTACT</a>
+    </router-link>
+  </ul>
+<!-- #dropdown-menu --></div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    showMenu: function () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
